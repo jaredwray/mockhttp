9a5f8f1c3f27bc7133a3ae3350d536b2d6e247d3
Jared Wray <me@jaredwray.com>
2024-11-24 10:48:14 -0800

    server is working

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,49 @@
 // Import necessary modules
 import Fastify from 'fastify';
 import { fastifySwagger } from '@fastify/swagger';
-import Redoc from 'redoc';
+import { fastifySwaggerUi } from '@fastify/swagger-ui';
+import { FastifyRequest } from 'fastify';
+
+type StatusRequest = FastifyRequest<{ Params: { code: string } }>;
 
 const fastify = Fastify({ logger: true });
 
 // Set up Swagger for API documentation
 fastify.register(fastifySwagger, {
-  swagger: {
-    info: {
-      title: 'HTTPBin Replacement API',
-      description: 'A replacement for httpbin built using Fastify, TypeScript, and Redoc',
-      version: '1.0.0',
+    swagger: {
+      info: {
+        title: 'HTTPBin Replacement API',
+        description: 'A replacement for httpbin built using Fastify, TypeScript, and ReDoc',
+        version: '1.0.0',
+      },
+      consumes: ['application/json'],
+      produces: ['application/json'],
     },
-    consumes: ['application/json'],
-    produces: ['application/json'],
-  },
-});
+  });
+
+  fastify.register(fastifySwaggerUi, {
+    routePrefix: '/docs',
+    uiConfig: {
+      docExpansion: 'none',
+      deepLinking: false,
+    },
+    uiHooks: {
+      onRequest: function (_request, _reply, next) {
+        next();
+      },
+      preHandler: function (_request, _reply, next) {
+        next();
+      },
+    },
+    staticCSP: true,
+    transformSpecification: (swaggerObject, _request, _reply) => {
+      return swaggerObject;
+    },
+    transformSpecificationClone: true,
+  });
 
 // Define routes to mimic HTTPBin functionality
-fastify.get('/get', async (request, reply) => {
+fastify.get('/get', async (request) => {
   return {
     method: 'GET',
     headers: request.headers,
@@ -27,7 +51,7 @@ fastify.get('/get', async (request, reply) => {
   };
 });
 
-fastify.post('/post', async (request, reply) => {
+fastify.post('/post', async (request) => {
   return {
     method: 'POST',
     headers: request.headers,
@@ -35,7 +59,7 @@ fastify.post('/post', async (request, reply) => {
   };
 });
 
-fastify.put('/put', async (request, reply) => {
+fastify.put('/put', async (request) => {
   return {
     method: 'PUT',
     headers: request.headers,
@@ -43,7 +67,7 @@ fastify.put('/put', async (request, reply) => {
   };
 });
 
-fastify.delete('/delete', async (request, reply) => {
+fastify.delete('/delete', async (request) => {
   return {
     method: 'DELETE',
     headers: request.headers,
@@ -51,7 +75,7 @@ fastify.delete('/delete', async (request, reply) => {
   };
 });
 
-fastify.get('/status/:code', async (request, reply) => {
+fastify.get('/status/:code', async (request: StatusRequest, reply) => {
   const { code } = request.params;
   reply.code(Number(code));
   return {
@@ -60,18 +84,33 @@ fastify.get('/status/:code', async (request, reply) => {
 });
 
 // Register Redoc for API documentation
-fastify.get('/redoc', async (request, reply) => {
-  const html = Redoc.generateHTML({
-    title: 'HTTPBin Replacement API',
-    specUrl: '/docs/json',
-  });
-  reply.type('text/html').send(html);
+fastify.get('/', async (_request, reply) => {
+  const redocHtml = `
+  <!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>{ mockhttp }</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Roboto:300,400,700">
+  <style>
+    body {
+      margin: 0;
+      padding: 0;
+    }
+  </style>
+</head>
+<body>
+  <redoc spec-url='/docs/json'></redoc>
+  <script src="https://cdn.redoc.ly/redoc/latest/bundles/redoc.standalone.js"></script>
+</body>
+</html>`
+  reply.type('text/html').send(redocHtml);
 });
 
 // Start the Fastify server
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000, host: '0.0.0.0' });
+    await fastify.listen({ port: 3000, host: '127.0.0.1' });
     fastify.log.info(`Server is running at http://localhost:3000`);
   } catch (err) {
     fastify.log.error(err);

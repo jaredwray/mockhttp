09386f9daaddc43529d0a36098111a0357475942
Jared Wray <me@jaredwray.com>
2024-12-31 12:50:30 -0800

    fixing sitemap

--- src/routes/sitemap.ts
+++ src/routes/sitemap.ts
@@ -1,5 +1,3 @@
-import fs from 'node:fs';
-import path from 'node:path';
 import {type FastifyInstance, type FastifyRequest, type FastifyReply} from 'fastify';
 
 export const sitemapRoute = (fastify: FastifyInstance) => {
@@ -7,15 +5,11 @@ export const sitemapRoute = (fastify: FastifyInstance) => {
 		try {
 			const host = _request.headers.host ?? 'mockhttp.org';
 			const isSecure = _request.headers['x-forwarded-proto']?.includes('https');
-			const packageJsonPath = path.resolve('package.json');
-			const packageStats = fs.statSync(packageJsonPath);
-			const packageDate = packageStats.mtime.toISOString().split('T')[0];
 			const protocol = isSecure ? 'https' : 'http';
 			const xml = `<?xml version="1.0" encoding="UTF-8"?>
                             <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
                                 <url>
                                     <loc>${protocol}://${host}/</loc>
-                                    <lastmod>${packageDate}</lastmod>
                                     <changefreq>monthly</changefreq>
                                     <priority>1.0</priority>
                                 </url>

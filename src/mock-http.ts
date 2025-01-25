a57deba0e9decf1c44227e4f267bebcf9433f489
Jared Wray <me@jaredwray.com>
2025-01-25 15:05:02 -0800

    adding in options and server

--- src/mock-http.ts
+++ src/mock-http.ts
@@ -1,4 +1,14 @@
 import {Hookified, type HookifiedOptions} from 'hookified';
+import {fastify, type FastifyInstance} from 'fastify';
+
+// eslint-disable-next-line unicorn/prevent-abbreviations
+export type HttpBinOptions = {
+	httpMethods?: boolean;
+	redirects?: boolean;
+	requestInspection?: boolean;
+	responseInspection?: boolean;
+	statusCodes?: boolean;
+};
 
 export type MockHttpOptions = {
 	/**
@@ -20,7 +30,7 @@ export type MockHttpOptions = {
 	/**
 	 * Whether to use Swagger UI. Defaults to true.
 	 */
-	httpBin?: boolean;
+	httpBin?: HttpBinOptions | boolean;
 	/**
 	 * Hookified options.
 	 */
@@ -28,8 +38,80 @@ export type MockHttpOptions = {
 };
 
 export class MockHttp extends Hookified {
+	private _port = 3000;
+	private _host = '0.0.0.0';
+	private _helmet = true;
+	private _apiDocs = true;
+	private _httpBin: boolean | HttpBinOptions = true;
+	private readonly _server = fastify();
+
 	constructor(options?: MockHttpOptions) {
 		super(options?.hookOptions);
+
+		if (options?.port) {
+			this._port = options.port;
+		}
+
+		if (options?.host) {
+			this._host = options.host;
+		}
+
+		if (options?.helmet !== undefined) {
+			this._helmet = options.helmet;
+		}
+
+		if (options?.apiDocs !== undefined) {
+			this._apiDocs = options.apiDocs;
+		}
+
+		if (options?.httpBin !== undefined) {
+			this._httpBin = options.httpBin;
+		}
+	}
+
+	public get port(): number {
+		return this._port;
+	}
+
+	public set port(port: number) {
+		this._port = port;
+	}
+
+	public get host(): string {
+		return this._host;
+	}
+
+	public set host(host: string) {
+		this._host = host;
+	}
+
+	public get helmet(): boolean {
+		return this._helmet;
+	}
+
+	public set helmet(helmet: boolean) {
+		this._helmet = helmet;
+	}
+
+	public get apiDocs(): boolean {
+		return this._apiDocs;
+	}
+
+	// eslint-disable-next-line unicorn/prevent-abbreviations
+	public set apiDocs(apiDocs: boolean) {
+		this._apiDocs = apiDocs;
+	}
+
+	public get httpBin(): boolean | HttpBinOptions {
+		return this._httpBin;
+	}
+
+	public set httpBin(httpBinary: boolean | HttpBinOptions) {
+		this._httpBin = httpBinary;
+	}
+
+	public get server(): FastifyInstance {
+		return this._server;
 	}
 }
 
--- test/mock-http.test.ts
+++ test/mock-http.test.ts
@@ -10,4 +10,48 @@ describe('MockHttp', () => {
 		// eslint-disable-next-line new-cap
 		expect(new mockhttp()).toBeInstanceOf(MockHttp);
 	});
+
+	test('should be able to pass in options', () => {
+		const options = {
+			port: 8080,
+			host: 'localhost',
+			helmet: true,
+			apiDocs: true,
+			httpBin: true,
+		};
+		const mock = new MockHttp(options);
+
+		expect(mock.host).toBe('localhost');
+		expect(mock.helmet).toBe(true);
+		expect(mock.apiDocs).toBe(true);
+		expect(mock.httpBin).toBe(true);
+	});
+
+	test('should be able to set options', () => {
+		const mock = new MockHttp();
+
+		expect(mock.port).toBe(3000);
+		expect(mock.host).toBe('0.0.0.0');
+		expect(mock.helmet).toBe(true);
+		expect(mock.apiDocs).toBe(true);
+		expect(mock.httpBin).toBe(true);
+
+		mock.port = 3001;
+		mock.host = 'localhost';
+		mock.helmet = false;
+		mock.apiDocs = false;
+		mock.httpBin = false;
+
+		expect(mock.port).toBe(3001);
+		expect(mock.host).toBe('localhost');
+		expect(mock.helmet).toBe(false);
+		expect(mock.apiDocs).toBe(false);
+		expect(mock.httpBin).toBe(false);
+	});
+
+	test('should be able to get the Fastify server', () => {
+		const mock = new MockHttp();
+
+		expect(mock.server).toBeDefined();
+	});
 });
